d4fac92733b337faf56a0dcf4b51c3567523e50e
Joost Vandorp <joostvandorp@gmail.com>
2013-01-07 17:30:30 -0500

    removed information from versions.cfg

--- storefront/urls/product/items.py
+++ storefront/urls/product/items.py
@@ -6,6 +6,7 @@ from storefront.views.product.items import ItemDetail
 
 urlpatterns = patterns(
     '',
+    
     url(r'^(?P<slug>[-\w]+)/$',
 	        ItemDetail.as_view(),
 	        name="storefront_item_detail"),
