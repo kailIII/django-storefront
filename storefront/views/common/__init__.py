9af07eae1ba77bbb15dea6d42cf686b3fffcc52a
Joost Vandorp <joostvandorp@gmail.com>
2013-01-14 12:49:08 -0500

    Improved item_details

--- storefront/views/common/__init__.py
+++ storefront/views/common/__init__.py
@@ -3,7 +3,10 @@ from storefront.models.product.items import Item
 from storefront.models.frontend.banners import Banner
 
 from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
+
+#for contact form only
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 class HomePage( TemplateView ):
     
@@ -30,7 +33,29 @@ class AboutPage( TemplateView ):
 class ContactPage( TemplateView ):
     
     template_name="storefront/contact.html"
-	
+    
+    def contact(request):
+        if request.method == 'POST': # If the form has been submitted...
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                subject = form.cleaned_data['subject']
+                message = form.cleaned_data['message']
+                sender = form.cleaned_data['sender']
+                cc_myself = form.cleaned_data['cc_myself']
+                
+                recipients =['COMPANY_EMAIL'] #GET THIS FROM SETTINGS!!!
+                if cc_myself:
+                    recipients.append(sender)
+                    
+                from django.core.mail import send_mail
+                send_mail(subject, message, sender, recipients)
+                return HttpResponseRedirect('/thanks/')
+        else:
+            form = ContactForm()
+        return render(request, 'storefront/contact.html', {
+                'form': form,
+        })
+            
     def get_context_data(self, **kwargs):
         context = super(ContactPage, self).get_context_data(**kwargs)
         return context
